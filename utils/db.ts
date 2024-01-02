abd0cad9da9c16eb4299627c8a4daa3383ceb2cc
Aung Myint Myat Oo <solidifyarmor@gmail.com>
2024-01-02 23:03:51 +0630

    feat: setup prisma db client

---
+++ utils/db.ts
@@ -0,0 +1,13 @@
+import { PrismaClient } from '@prisma/client'
+
+const globalForPrisma = globalThis as {
+  prisma?: PrismaClient
+}
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ['query'],
+  })
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
