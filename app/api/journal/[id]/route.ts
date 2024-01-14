570484a98cb891620565900acffc806e67908b5a
Aung Myint Myat Oo <solidifyarmor@gmail.com>
2024-01-14 23:15:28 +0630

    Creating & Updating Journal Entries

--- app/api/journal/[id]/route.ts
+++ app/api/journal/[id]/route.ts
@@ -1,5 +1,21 @@
 import { getUserByClerkId } from '@/utils/auth'
+import { prisma } from '@/utils/db'
+import { NextResponse } from 'next/server'
 
-export const PATCH = async (request, { params }) => {
+export const PATCH = async (request: Request, { params }) => {
+  const { content } = await request.json()
   const user = await getUserByClerkId()
+  const updatedEntry = await prisma.journalEntry.update({
+    where: {
+      userId_id: {
+        userId: user.id,
+        id: params.id,
+      },
+    },
+    data: {
+      content,
+    },
+  })
+
+  return NextResponse.json({ data: updatedEntry })
 }
--- utils/api.ts
+++ utils/api.ts
@@ -1,5 +1,18 @@
 const createURL = (path) => window.location.origin + path
 
+export const updateEntry = async (id, content) => {
+  const res = await fetch(
+    new Request(createURL(`/api/journal/${id}`), {
+      method: 'PATCH',
+      body: JSON.stringify({ content }),
+    })
+  )
+  if (res.ok) {
+    const data = await res.json()
+    return data.data
+  }
+}
+
 export const createNewEntry = async () => {
   const res = await fetch(
     new Request(createURL('/api/journal'), {
